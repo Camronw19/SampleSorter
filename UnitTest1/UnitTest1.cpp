ce2706d7a8497fbced4f26105bd72863094319fc
Camron Walsh <camronwalsh@gmail.com>
2024-05-13 18:26:04 -0400

    Basic Testing Project

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "FileUtils.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
 
 namespace UnitTest1
 {
@@ -11,9 +14,7 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
-			const std::string name = "test"; 
-			const std::string name2 = "test"; 
-			Assert::AreEqual(name, name2);
+			int a = 1; 
 		}
 	};
 }
