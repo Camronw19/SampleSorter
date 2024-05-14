5723d29391ac802c3b5cd75ebcb4d4318a35c315
Camron Walsh <camronwalsh@gmail.com>
2024-05-14 11:23:23 -0400

    Added additional Data Modeling Tests

--- UnitTest1/DataModels/SampleInfoTests.cpp
+++ UnitTest1/DataModels/SampleInfoTests.cpp
@@ -14,17 +14,53 @@ namespace UnitTest1
 		TEST_METHOD(Constructor_DefualtConstructsValidObject)
 		{
 			SampleInfoDataModel sample_info; 
-			Assert::IsTrue(sample_info.State().hasType(ModelIdentifiers::SAMPLE_INFO));
+			Assert::IsTrue(sample_info.getState().hasType(ModelIdentifiers::SAMPLE_INFO));
 		}
 
 		TEST_METHOD(Constructor_CopyContructsValidObject)
 		{
 			SampleInfoDataModel original; 
-			original.SetName(juce::String("TEST"));
+			original.setName(juce::String("TEST"));
 
 			SampleInfoDataModel copy(original);
 
-			Assert::AreEqual(original.Name().toStdString(), copy.Name().toStdString());
+			Assert::AreEqual(original.getName().toStdString(), copy.getName().toStdString());
+			Assert::IsTrue(original.getState() == copy.getState());
+		}
+
+		TEST_METHOD(Setter_SetState)
+		{
+			juce::ValueTree value_tree(ModelIdentifiers::SAMPLE_INFO); 
+			SampleInfoDataModel sample_info(value_tree); 
+
+			Assert::IsTrue(value_tree == sample_info.getState()); 
+		}
+
+		TEST_METHOD(Setter_SetName)
+		{
+			SampleInfoDataModel sample_info; 
+			juce::String name = "TEST"; 
+			sample_info.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_info.getName().toStdString());
+		}
+
+		TEST_METHOD(Setter_SetName_EmptyString)
+		{
+			SampleInfoDataModel sample_info;
+			juce::String name = "";
+			sample_info.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_info.getName().toStdString());
+		}
+
+		TEST_METHOD(Setter_SetName_SpecialCharacters)
+		{
+			SampleInfoDataModel sample_info;
+			juce::String name = "Tëst!@#$%^&*()";
+			sample_info.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_info.getName().toStdString());
 		}
 	};
 }
--- UnitTest1/DataModels/SampleLibraryTests.cpp
+++ UnitTest1/DataModels/SampleLibraryTests.cpp
@@ -14,17 +14,52 @@ namespace UnitTest1
 		TEST_METHOD(Constructor_DefualtConstructsValidObject)
 		{
 			SampleLibraryDataModel sample_library; 
-			Assert::IsTrue(sample_library.State().hasType(ModelIdentifiers::SAMPLE_LIBRARY));
+			Assert::IsTrue(sample_library.getState().hasType(ModelIdentifiers::SAMPLE_LIBRARY));
 		}
 
 		TEST_METHOD(Constructor_CopyContructsValidObject)
 		{
 			SampleLibraryDataModel original;
-			original.SetName(juce::String("TEST"));
+			original.setName(juce::String("TEST"));
 
 			SampleLibraryDataModel copy(original);
 
-			Assert::AreEqual(original.Name().toStdString(), copy.Name().toStdString());
+			Assert::AreEqual(original.getName().toStdString(), copy.getName().toStdString());
+		}
+
+		TEST_METHOD(Setter_SetState)
+		{
+			juce::ValueTree value_tree(ModelIdentifiers::SAMPLE_LIBRARY);
+			SampleLibraryDataModel sample_library(value_tree);
+
+			Assert::IsTrue(value_tree == sample_library.getState());
+		}
+
+		TEST_METHOD(Setter_SetName)
+		{
+			SampleLibraryDataModel sample_library;
+			juce::String name = "TEST";
+			sample_library.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_library.getName().toStdString());
+		}
+
+		TEST_METHOD(Setter_SetName_EmptyString)
+		{
+			SampleLibraryDataModel sample_library;
+			juce::String name = "";
+			sample_library.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_library.getName().toStdString());
+		}
+
+		TEST_METHOD(Setter_SetName_SpecialCharacters)
+		{
+			SampleLibraryDataModel sample_library;
+			juce::String name = "Tëst!@#$%^&*()";
+			sample_library.setName(name);
+
+			Assert::AreEqual(name.toStdString(), sample_library.getName().toStdString());
 		}
 	};
 }
