ca4c0adcdfd8c532ec17e444e69432d6451ccaec
Camron Walsh <camronwalsh@gmail.com>
2024-05-16 12:25:14 -0400

    Added FileListTable Testing File

---
+++ UnitTest1/GUI/FileListTableTests.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "FileDisplayComponents.h"
+#include "SampleInfo.h"
+#include "SampleLibrary.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+namespace UnitTest1
+{
+	TEST_CLASS(FileListTableTests)
+	{
+	public:
+
+		TEST_METHOD(Test1)
+		{
+
+		}
+	};
+}
