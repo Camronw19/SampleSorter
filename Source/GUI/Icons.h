a827dd58f0b9257905d6e1f1172bc51b8c7c06c4
Camron Walsh <camronwalsh@gmail.com>
2024-05-24 08:41:35 -0400

    Created Icon Files and Filter Icon

---
+++ Source/GUI/Icons.h
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    Icons.h
+    Created: 24 May 2024 7:55:00am
+    Author:  camro
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include <memory>
+
+std::unique_ptr<juce::DrawablePath> createFilterIcon(); 
