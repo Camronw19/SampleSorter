dc63b4d4f63e3247af5155088bce3cdff8efbd7f
Camron Walsh <camronwalsh@gmail.com>
2024-06-09 13:28:08 -0400

    Small Refactoring

--- Source/GUI/Icons.cpp
+++ Source/GUI/Icons.cpp
@@ -3,7 +3,7 @@
 
     Icons.cpp
     Created: 24 May 2024 7:55:00am
-    Author:  camro
+    Author: Camron Walsh 
 
   ==============================================================================
 */
@@ -13,19 +13,16 @@
 
 std::unique_ptr<juce::DrawablePath> createFilterIcon()
 {
-    // Create a DrawablePath object to hold our icon's path
     auto drawable_path = std::make_unique<juce::DrawablePath>();
     juce::Path path; 
 
-    // Define the bar heights and initial width
+    float x = 0.0f;
+    float y = 0.0f;
+    float gap = 20.0f;
     float bar_height = 7.5f;
     float initial_width = 100.0f;
     float width_decrement = 30.0f;
-    float gap = 20.0f;
-    float y = 0.0f;
-    float x = 0.0f;
 
-    // Add the three bars to the path
     for (int i = 0; i < 3; ++i)
     {
         float currentWidth = initial_width - (i * width_decrement);
@@ -34,7 +31,6 @@ std::unique_ptr<juce::DrawablePath> createFilterIcon()
         y += bar_height + gap;
     }
 
-    // Set the path and color of drawable
     drawable_path->setPath(path);
     drawable_path->setFill(drawable_path->getLookAndFeel().findColour(AppColors::OnBackground));
 
